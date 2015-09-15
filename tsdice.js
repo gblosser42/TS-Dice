1553e93c06ce7dc4b504e33a446f6c10e12d895f
Glen Blosser <gblosser@dotomi.com>
2015-09-15 14:28:43 -0500

    Added inititiave tracker

--- tsdice.js
+++ tsdice.js
@@ -11,6 +11,10 @@ var cid = 1;
 var pid;
 var clientMessageStatus = {};
 var storyteller;
+var tracker = {};
+var back = [];
+var forward = [];
+var output = '';
 
 var exaltedDice = function (message) {
 	var dice = message.match(/([0-9]+)e/);
@@ -208,6 +212,263 @@ var welcome = function (params) {
 	}, function (err, response) {});
 };
 
+var initiativeHandler = function (params) {
+	var raw = params.msg.substr(1);
+	var parts = raw.split(' ');
+	var command = parts[0];
+	var highest = -9999999;
+	var active = [];
+	var sendMessage = function (msg) {
+		cl.send('clientupdate', {clid: clid, client_nickname: 'Initiative'}, function () {
+			cl.send('sendtextmessage', {
+				targetmode: 2, msg: msg
+			}, function (err, response) {
+				cl.send('clientupdate', {clid: clid, client_nickname: 'Dice Roller'}, function () {});
+			});
+		});
+	};
+	var reset = function () {
+		var oldTracker = JSON.parse(JSON.stringify(tracker));
+		tracker = {};
+		back.push(function (un) {
+			if (un==='undo') {
+				tracker = oldTracker;
+				sendMessage('Undoing reset');
+			} else {
+				tracker = {};
+				sendMessage('Redoing reset');
+			}
+		});
+	};
+	var next = function () {
+		Object.keys(tracker).forEach(function (actor) {
+			actor = tracker[actor];
+			if (!actor.acted) {
+				if (actor.initiative > highest) {
+					highest = actor.initiative;
+					active = [actor];
+				} else if (actor.initiative === highest) {
+					active.push(actor);
+				}
+			}
+		});
+		if (active.length > 0) {
+			output = highest + ':';
+			active.forEach(function (actor) {
+				actor.acted = true;
+				output += ' ' + actor.name + ',';
+			});
+			output = output.replace(/,$/, '');
+			sendMessage(output);
+			back.push(function (un) {
+				if (un === 'undo') {
+					active.forEach(function (actor) {
+						actor.acted = false;
+					});
+					sendMessage('Undoing next turn');
+				} else {
+					active.forEach(function (actor) {
+						actor.acted = true;
+					});
+					sendMessage('Redoing next turn');
+				}
+			});
+		} else {
+			sendMessage('NEW TURN');
+			Object.keys(tracker).forEach(function (actorId) {
+				var actor = tracker[actorId];
+				actor.acted = false;
+			});
+			list();
+			back.push(function (un) {
+				if (un === 'undo') {
+					Object.keys(tracker).forEach(function (actorId) {
+						var actor = tracker[actorId];
+						actor.acted = true;
+					});
+					sendMessage('Undoing New Turn');
+				} else {
+					Object.keys(tracker).forEach(function (actorId) {
+						var actor = tracker[actorId];
+						actor.acted = false;
+					});
+					sendMessage('NEW TURN');
+					list();
+					sendMessage('Redoing New Turn');
+				}
+			});
+		}
+	};
+	var add = function () {
+		var name = parts[1];
+		var actor = {
+			name: name,
+			initiative: parseInt(parts[2], 10) || 0,
+			acted: false
+		};
+		tracker[name] = actor;
+		back.push(function (un) {
+			if (un === 'undo') {
+				delete tracker[name];
+				sendMessage('Deleting ' + name);
+			} else {
+				tracker[name] = actor;
+				sendMessage('Readding ' + name);
+			}
+		});
+	};
+	var remove = function () {
+		var actor = tracker[parts[1]];
+		var name = actor.name;
+		if (!!actor) {
+			delete tracker[name];
+			back.push(function (un) {
+				if (un === 'undo') {
+					tracker[name] = actor;
+					sendMessage('Re-adding ' + name);
+				} else {
+					delete tracker[name];
+					sendMessage('Re-deleting ' + name);
+				}
+			});
+		}
+	};
+	var list = function () {
+		var output = [],
+		toPrint = '';
+		Object.keys(tracker).forEach(function (name) {
+			output.push(tracker[name].initiative + ' ' + name);
+		});
+		output.sort(function (a, b) { return parseFloat(b.split(' ')[0]) - parseFloat(a.split(' ')[0]) });
+		output.forEach(function (val) {
+			toPrint += val + '\n';
+		});
+		sendMessage('\n' + toPrint.replace(/\n$/, ''));
+	};
+	var set = function () {
+		var oldValue = tracker[parts[1]].initiative;
+		var newValue = parseInt(parts[2], 10);
+		var name = parts[1];
+		tracker[name].initiative = newValue;
+		back.push(function (un) {
+			if (un==='undo') {
+				tracker[name].initiative = oldValue;
+				sendMessage('Reset ' + name + '\'s initiative to ' + oldValue);
+			} else {
+				tracker[name].initiative = newValue;
+				sendMessage('Re-set ' + name + '\'s initiative to ' + newValue);
+			}
+		});
+	};
+	var modify = function () {
+		var oldValue = tracker[parts[1]].initiative;
+		var newValue = oldValue + parseInt(parts[2], 10);
+		var name = parts[1];
+		tracker[name].initiative = newValue;
+		back.push(function (un) {
+			if (un==='undo') {
+				tracker[name].initiative = oldValue;
+				sendMessage('Reset ' + name + '\'s initiative to ' + oldValue);
+			} else {
+				tracker[name].initiative = newValue;
+				sendMessage('Re-set ' + name + '\'s initiative to ' + newValue);
+			}
+		});
+	};
+	var withering = function () {
+		var aName = parts[1];
+		var dName = parts[2];
+		var attackerOldValue = tracker[aName].initiative;
+		var attackerNewValue = attackerOldValue + parseInt(parts[3], 10) + 1;
+		var defenderOldValue = tracker[dName].initiative;
+		var defenderNewValue = defenderOldValue - parseInt(parts[3], 10);
+		if (defenderNewValue <= 0) {
+			attackerNewValue += 5;
+			sendMessage(dName + ' is CRASHED');
+		}
+		tracker[aName].initiative = attackerNewValue;
+		tracker[dName].initiative = defenderNewValue;
+		back.push(function (un) {
+			if (un==='undo') {
+				tracker[aName].initiative = attackerOldValue;
+				tracker[dName].initiative = defenderOldValue;
+				sendMessage('Undoing withering attack');
+			} else {
+				tracker[aName].initiative = attackerNewValue;
+				tracker[dName].initiative = defenderNewValue;
+				sendMessage('Redoing withering attack');
+			}
+		});
+	};
+	var undo = function() {
+		var func;
+		if (back.length > 0) {
+			func = back.pop();
+			func('undo');
+			forward.push(func);
+		} else {
+			sendMessage('Nothing to Undo');
+		}
+	};
+	var redo = function() {
+		var func;
+		if (forward.length > 0) {
+			func = forward.pop();
+			func('redo');
+			back.push(func);
+		} else {
+			sendMessage('Nothing to Redo');
+		}
+	};
+	var check = function () {
+		var name = parts[1];
+		var output = tracker[name].initiative + ' ' + name;
+		sendMessage(output);
+	};
+	try {
+		switch (command) {
+			case 'reset':
+				reset();
+				break;
+			case 'next':
+				next();
+				break;
+			case 'add':
+				add();
+				break;
+			case 'list':
+				list();
+				break;
+			case 'check':
+				check();
+				break;
+			case 'set':
+				set();
+				break;
+			case 'modify':
+				modify();
+				break;
+			case 'withering':
+				withering();
+				break;
+			case 'delete':
+			case 'remove':
+				remove();
+				break;
+			case 'undo':
+				undo();
+				break;
+			case 'redo':
+				redo();
+				break;
+			case 'default':
+				sendMessage('Not Recognized Command');
+		}
+	} catch (e) {
+		sendMessage('INPUT ERROR');
+	}
+};
+
 var diceHandler = function (params) {
 	var msgParts = params.msg.toString().match(/\(([0-9].+?)\)/);
 	var result;
@@ -270,7 +531,12 @@ cl.send('login', {client_login_name: uid, client_login_password: config.password
 			cl.send('clientupdate', {clid: clid, client_nickname: 'Dice Roller'}, function (err, response) {
 				cl.send('servernotifyregister', {event: 'textchannel', id:0}, function(err, response){
 					cl.on('textmessage', function (params) {
-						diceHandler(params);
+						if (params.msg.match(/^!/)) {
+							initiativeHandler(params);
+						}
+						else {
+							diceHandler(params);
+						}
 					});
 					keepAlive();
 				});
@@ -300,13 +566,23 @@ cl.send('login', {client_login_name: uid, client_login_password: config.password
 						cl.send('clientmove', {clid: clid, cid: channel.cid}, function () {
 							cid = channel.cid;
 							cl.send('clientlist', {cid:cid}, function (err, response) {
-								response.forEach(function(client){
-									welcome({clid:client.clid});
-									welcome({clid:client.clid});
-									if (client.client_nickname.toLowerCase() === 'storyteller' || client.client_nickname.toLowerCase() === 'lord of chaos') {
-										storyteller = client.clid;
+								if (response) {
+									if (response.forEach) {
+										response.forEach(function (client) {
+											welcome({clid: client.clid});
+											welcome({clid: client.clid});
+											if (client.client_nickname.toLowerCase() === 'storyteller' || client.client_nickname.toLowerCase() === 'lord of chaos') {
+												storyteller = client.clid;
+											}
+										});
+									} else {
+										welcome({clid: response.clid});
+										welcome({clid: response.clid});
+										if (response.client_nickname.toLowerCase() === 'storyteller' || response.client_nickname.toLowerCase() === 'lord of chaos') {
+											storyteller = response.clid;
+										}
 									}
-								});
+								}
 							});
 						});
 					}
